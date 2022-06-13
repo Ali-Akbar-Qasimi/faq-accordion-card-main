f1feb25ca222ce7fdf848d9d0a06e10f82042e88
Ali Akbar Qaismi <aliaqasimi90@gmail.com>
2022-06-14 07:02:23 +0800

    fixed some styles

--- js.js
+++ js.js
@@ -2,11 +2,25 @@ let questions = document.querySelectorAll('.question-container')
 console.log(questions);
 
 questions.forEach(question => {
-    question.addEventListener('click', function(e){
-        console.log(question.firstElementChild)
+    question.addEventListener('click', ()=>{
+        for(let i = 0 ; i < questions.length; i++){
+            questions[i].classList.remove('active')
+        }
         if(question.firstElementChild.checked === true){
             question.classList.toggle('active')
         }
-        
     });
 })
+
+// change the images 
+
+let womenImage = document.querySelector('.images img')
+window.addEventListener('resize',changeImages)
+changeImages()
+function changeImages(){
+    if(window.innerWidth > 786){
+        womenImage.src = './images/illustration-woman-online-mobile.svg'
+    }else{
+        womenImage.src = './images/illustration-woman-online-mobile.svg'
+    }
+}
